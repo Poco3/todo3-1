c9c3c854452b119ec2bf45f0898e3bdbf2849a2f
Poco3 <75761025+Poco3@users.noreply.github.com>
2021-04-18 19:53:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 'use strict';
 
 let num = 0;
-      
-      
+
+
 const todos = [];
 const todo = {
-   comment: '',
-   status: "作業中"
-    }
+    comment: '',
+    status: '作業中'
+}
 
-const output = document.getElementById("output");
+const output = document.getElementById('output');
 
 //関数addTask
 const addTask = () => {
-const input = document.getElementById('input').value;
-todo.comment = input
-todos.push(todo)
-displayTodos(todo)
-num++
+    const input = document.getElementById('input').value;
+    todo.comment = input
+    todos.push(todo);
+    displayTodos(todo);
+    num++
 }
 
 //関数displayTodos
 const displayTodos = (todo) => {
-const list = document.createElement("td");
-list.textContent = num;
+    const list = document.createElement('td');
+    list.textContent = num;
 
-const th = document.createElement('td');
-th.textContent = todo.comment
-input.value ='';
+    const th = document.createElement('td');
+    th.textContent = todo.comment
+    input.value = '';
 
-const stbtn1 = () =>{
-const btn1 = document.createElement("button");
-btn1.textContent = todo.status
-return btn1;
-}
+    const Status = () => {
+        const StatusButton = document.createElement('button');
+        StatusButton.textContent = todo.status
+        return StatusButton;
+    }
 
-const stbtn2 = () =>{
-const btn2 = document.createElement("button")
-btn2.textContent ='削除'
-return btn2;
-}
+    const Delete = () => {
+        const DeleteButton = document.createElement('button');
+        DeleteButton.textContent = '削除'
+        return DeleteButton;
+    }
 
 
 
-const tBody = document.getElementById('new-task')
-const tr = document.createElement('tr');
-tBody.appendChild(tr);
-tBody.appendChild(list);
-tBody.appendChild(th);
-tBody.appendChild(stbtn1());
-tBody.appendChild(stbtn2());
+    const table = document.getElementById('new-task');
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
+    table.appendChild(list);
+    table.appendChild(th);
+    table.appendChild(Status());
+    table.appendChild(Delete());
 };
 
 output.addEventListener('click', addTask,);
